8d48c67d2fd6c2b8508a234c0fec30446dff2696
Gerard de Brieder <smeevil@me.com>
2018-07-30 14:11:59 +0200

    rename hex

--- mix.exs
+++ mix.exs
@@ -12,11 +12,11 @@ defmodule ControlPlus.Mixfile do
         tool: ExCoveralls
       ],
       preferred_cli_env: [
-        "coveralls": :test,
+        coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
         "coveralls.html": :test,
-        "vcr": :test,
+        vcr: :test,
         "vcr.delete": :test,
         "vcr.check": :test,
         "vcr.show": :test
