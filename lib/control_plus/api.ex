1d6bba483cfa88c161f7ed2cca125a04ce691fee
Rachid Al Maach <rachid@qraft.nl>
2019-01-02 09:14:08 +0100

    Don't pass default status

--- lib/control_plus/api.ex
+++ lib/control_plus/api.ex
@@ -37,7 +37,7 @@ defmodule ControlPlus.Api do
   @spec paginated_clients(keyword | nil) ::
           {:ok, %{total_pages: non_neg_integer, current_page: non_neg_integer, clients: [%ControlPlus.Client{}]}} |
           {:error, any}
-  def paginated_clients(options \\ [page: 1, limit: 30, status: :valid]) do
+  def paginated_clients(options \\ [page: 1, limit: 30]) do
     case ControlPlus.ApiClient.fetch(
            :clients_list,
            params: [
